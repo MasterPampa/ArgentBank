b7b3981aa5011ebfc2a8daa83160d72091f202bb
Master Pampa <styven.munhurrun@gmail.com>
2023-08-08 16:41:43 +0200

    page signin et composants pret pour integration

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/home/index'
 import Signin from './pages/signin';
+import User from './pages/user';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -12,6 +13,7 @@ root.render(
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<Signin />} />
+        <Route path="/user" element={<User />} />
       </Routes>
     </Router>
   </React.StrictMode>,
---
+++ my-app/src/pages/user/index.jsx
@@ -0,0 +1,14 @@
+import "./index.css"
+import Header from "../../components/header/Header"
+import Footer from "../../components/footer/Footer"
+
+function User () {
+    return(
+        <div className="wrapper">
+            <Header />
+            <Footer />
+        </div>
+    )
+}
+
+export default User
