28d409cc0b4514c1458b2e05b076519888c1093d
Master Pampa <styven.munhurrun@gmail.com>
2023-08-09 11:18:51 +0200

    Site statique converti en react complet

--- my-app/src/components/profile/Profile.jsx
+++ my-app/src/components/profile/Profile.jsx
@@ -1,11 +1,48 @@
 import "./Profile.css"
 
 function Profile () {
-    <main className="background">
-        <div className="profile">
-            <h1>Welcome back </h1>
-        </div>
-    </main>
+    return(
+        <main className="background">
+            <div className="profile">
+                <h1>Welcome back
+                    <br />
+                    $Username
+                </h1>
+                <button className="profile__button">Edit Name</button>
+            </div>
+            <h2 class="sr-only">Accounts</h2>
+            <section class="account">
+                <div class="account__wrapper">
+                    <h3 class="account__wrapper__title">Argent Bank Checking (x8349)</h3>
+                    <p class="account__wrapper__amount">$AmountChecking</p>
+                    <p class="account__wrapper__description">Available Balance</p>
+                </div>
+                <div class="account__wrapperButton">
+                    <button class="transactionButton">View transactions</button>
+                </div>
+            </section>
+            <section class="account">
+                <div class="account__wrapper">
+                    <h3 class="account__wrapper__title">Argent Bank Savings (x8349)</h3>
+                    <p class="account__wrapper__amount">$AmountSavings</p>
+                    <p class="account__wrapper__description">Available Balance</p>
+                </div>
+                <div class="account__wrapperButton">
+                    <button class="transactionButton">View transactions</button>
+                </div>
+            </section>
+            <section class="account">
+                <div class="account__wrapper">
+                    <h3 class="account__wrapper__title">Argent Bank Credit Card (x8349)</h3>
+                    <p class="account__wrapper__amount">$AmountCreditCard</p>
+                    <p class="account__wrapper__description">Available Balance</p>
+                </div>
+                <div class="account__wrapperButton">
+                    <button class="transactionButton">View transactions</button>
+                </div>
+            </section>
+        </main>
+    )
 }
 
 export default Profile
