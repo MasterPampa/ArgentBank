d7a5d3f7d9e2cbd4116ad8acea5deefa79f7fec8
Master Pampa <styven.munhurrun@gmail.com>
2023-08-13 14:22:46 +0200

    Ajout de js-cookies et l'option remember me

--- my-app/src/components/connect/Connect.jsx
+++ my-app/src/components/connect/Connect.jsx
@@ -4,6 +4,7 @@ import { login } from '../actions/authActions';
 import { getProfile } from '../actions/getProfileActions'
 import "./Connect.css"
 import { useNavigate } from 'react-router-dom';
+import Cookies from 'js-cookie'; // Import js-cookie library
 
 const Connect = () => {
     const dispatch = useDispatch();
@@ -12,11 +13,30 @@ const Connect = () => {
 
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [rememberMe, setRememberMe] = useState(false);
 
+   
     const handleSubmit = async e => {
         e.preventDefault();
+        
+        if (rememberMe) {
+            Cookies.set('rememberMe', JSON.stringify({ username, password}, { expires: 7 }))
+        } else {
+            Cookies.remove('rememberMe')
+        }
+
         dispatch(login(username, password));
-      }
+    }
+
+    useEffect(() => {
+        const rememberMeCookie = Cookies.get('rememberMe');
+        if (rememberMeCookie) {
+            setRememberMe(!rememberMe) ;
+            const { username, password } = JSON.parse(rememberMeCookie);
+            setUsername(username);
+            setPassword(password);
+        }
+    }, []);
     
     useEffect(() => {
         if (isAuthenticated === true) {
@@ -34,14 +54,14 @@ const Connect = () => {
                 <form onSubmit={handleSubmit}>
                     <div className="input-wrapper">
                         <label htmlFor="username">Username</label>
-                        <input type="text" id="username" onChange={e => setUsername(e.target.value)}/>
+                        <input type="text" id="username" onChange={e => setUsername(e.target.value)} value={username}/>
                     </div>
                     <div className="input-wrapper">
                         <label htmlFor="password">Password</label>
-                        <input type="password" id="password" onChange={e => setPassword(e.target.value)} autoComplete="true"/>
+                        <input type="password" id="password" onChange={e => setPassword(e.target.value)} autoComplete="true" value={password}/>
                     </div>
                     <div className="input-remember">
-                        <input type="checkbox" id="remember-me"/>
+                        <input type="checkbox" id="remember-me" checked={rememberMe} onChange={() => setRememberMe(!rememberMe)}/>
                         <label htmlFor="remember-me">Remember me</label>
                     </div>
                     <button type="submit" className="sign-in-button">Sign In</button>
