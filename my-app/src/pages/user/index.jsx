9df601cc7b30916e65d55569144fc502105f8bfc
Master Pampa <styven.munhurrun@gmail.com>
2023-08-13 13:35:06 +0200

    verification connexion avant page user

--- my-app/src/pages/user/index.jsx
+++ my-app/src/pages/user/index.jsx
@@ -1,16 +1,27 @@
-import "./index.css"
-import Header from "../../components/header/Header"
-import Footer from "../../components/footer/Footer"
-import Profile from "../../components/profile/Profile"
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import Header from '../../components/header/Header';
+import Footer from '../../components/footer/Footer';
+import Profile from '../../components/profile/Profile';
 
-function User () {
-    return(
+function User() {
+    const isAuthenticated = useSelector(state => state.isAuthenticated);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (isAuthenticated === false) {
+            navigate('/');
+        }
+    }, [isAuthenticated, navigate]);
+
+    return (
         <div className="wrapper">
             <Header />
             <Profile />
             <Footer />
         </div>
-    )
+    );
 }
 
-export default User
+export default User;
